29ae5cb5d26629e9637f23a6633624ac589f77c8
Kai Sassnowski <me@kai-sassnowski.com>
2020-05-13 15:00:48 +0200

    Download source, not compiled binary

--- create-image-placeholder.rb
+++ create-image-placeholder.rb
@@ -1,8 +1,8 @@
 class CreateImagePlaceholder < Formula
   desc "A tiny command line application to generate base64 encoded image placeholders"
   homepage "https://github.com/ksassnowski/create-image-placeholder"
-  url "https://github.com/ksassnowski/create-image-placeholder/releases/download/v1.0.0/create-image-placeholder.tar.gz"
-  sha256 "5c89bff93c5761bb039af86d2ec37ceb7c5cefebfd238dbb34a25b6652b656b8"
+  url "https://github.com/ksassnowski/create-image-placeholder/archive/v1.0.1.tar.gz"
+  sha256 "272660594b5d10d6c08893272826ebbf5f9cca6ff90fd533075fc6846370f62f"
 
   depends_on "go" => :build
 
