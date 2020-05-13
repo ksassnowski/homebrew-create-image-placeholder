9855d6e1f01e335f869376b10a6cf31ca4197034
Kai Sassnowski <me@kai-sassnowski.com>
2020-05-13 15:03:53 +0200

    Use correct checksum

--- create-image-placeholder.rb
+++ create-image-placeholder.rb
@@ -2,7 +2,7 @@ class CreateImagePlaceholder < Formula
   desc "A tiny command line application to generate base64 encoded image placeholders"
   homepage "https://github.com/ksassnowski/create-image-placeholder"
   url "https://github.com/ksassnowski/create-image-placeholder/archive/v1.0.1.tar.gz"
-  sha256 "272660594b5d10d6c08893272826ebbf5f9cca6ff90fd533075fc6846370f62f"
+  sha256 "727fac4b379b929e72966cde2b83b2e5ff39968c31e433ee7050bd2d4543b53e"
 
   depends_on "go" => :build
 
